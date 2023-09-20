d6fec9549afb58b8b4da24e6f7747352bb238a26
HenryTang2702 <copsuilat1237890@gmail.com>
2023-09-20 20:41:52 +0930

    dsad

--- prac5/Abs/Abs.asm
+++ prac5/Abs/Abs.asm
@@ -2,3 +2,34 @@
 // (R0, R1 refer to RAM[0], and RAM[1], respectively.)
 
 // Put your code here.
+
+// Store input -> R0
+@R1
+D=M
+
+@R0
+M=D
+
+// If value is negative, negate it
+@R0
+D=M
+
+@NEGATIVE
+M=D
+
+@R0
+D=M
+
+@END
+D;JGE
+
+@R0
+M=-M
+@END
+// End of program
+(NEGATIVE)
+@END
+
+(END)
+@END
+0;JMP
