1a47b2922aeed1f930d4e2a6207c28ac5ffdaf10
HenryTang2702 <copsuilat1237890@gmail.com>
2023-09-27 03:15:40 +0930

    sdads

--- prac5/Abs/Abs.asm
+++ prac5/Abs/Abs.asm
@@ -1,63 +1,35 @@
-// // Calculates the absolute value of R1 and stores the result in R0.
-// // (R0, R1 refer to RAM[0], and RAM[1], respectively.)
+// Calculates the absolute value of R1 and stores the result in R0.
+// (R0, R1 refer to RAM[0], and RAM[1], respectively.)
 
-// // Put your code here.
+// Put your code here.
 
-// // Store input -> R0
-// @R1
-// D=M
-
-// @R0
-// M=D
-
-// // If value is negative, negate it
-// @R0
-// D=M
-
-// @NEGATIVE
-// M=D
-
-// @R0
-// D=M
-
-// @END
-// D;JGE
-
-// @R0
-// M=-M
-// @END
-// // End of program
-// (NEGATIVE)
-// @END
-
-// (END)
-// @END
-// 0;JMP
-
-// Abs.asm
-
-// R1 contains the number
-// R0 will contain the absolute value of the number
-
-// load R1 into D
+// Store input -> R0
 @R1
 D=M
 
-// check if D is negative
-@NEG
-D;JLT
-
-// if D is positive or zero, set R0 to D
 @R0
 M=D
+
+// If value is negative, negate it
+@R0
+D=M
+
+@NEGATIVE
+M=D
+
+@R0
+D=M
+
 @END
-0;JMP
+D;JGE
 
-// if D is negative, negate it and set R0 to the result
-(NEG)
-D=-D
 @R0
-M=D
+M=-M
+@END
+// End of program
+(NEGATIVE)
+@END
 
 (END)
-// End of program
+@END
+0;JMP
--- prac5/Abs/Abs01.tst
+++ prac5/Abs/Abs01.tst
@@ -1,15 +1,15 @@
-load Abs.asm,
-output-file Abs01.out,
-compare-to Abs01.cmp,
+// load Abs.asm,
+// output-file Abs01.out,
+// compare-to Abs01.cmp,
 
-set RAM[0] 0,   // Set R0
-set RAM[1] -3;   // Set R1 to a negative value
+// set RAM[0] 0,   // Set R0
+// set RAM[1] -3;   // Set R1 to a negative value
 
-repeat 100 {
-  ticktock;    // Run for 100 clock cycles
-}
+// repeat 100 {
+//   ticktock;    // Run for 100 clock cycles
+// }
 
-set RAM[1] -3,   // Restore arguments in case program used them
-set RAM[0] 0,   // Restore arguments in case program used them
+// set RAM[1] -3,   // Restore arguments in case program used them
+// set RAM[0] 0,   // Restore arguments in case program used them
 
-output;        // Output to file
+// output;        // Output to file
