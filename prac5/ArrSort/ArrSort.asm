2bea17f11ecc535d7daa3d93b7d5fca498401ffd
HenryTang2702 <copsuilat1237890@gmail.com>
2023-09-27 03:48:33 +0930

    sdads

--- prac5/ArrSort/ArrSort.asm
+++ prac5/ArrSort/ArrSort.asm
@@ -11,110 +11,82 @@
 // R2 contains the length of the array
 // R0 is the output to be set to -1 when the program finishes
 
-@R2
-D=M
-@LENGTH
-M=D
+// ArrSort.asm
+// Sorts an array in-place in ascending order using Bubble Sort.
+// Inputs:
+// - R1: RAM address of the first element in the array
+// - R2: Length of the array
+// Outputs:
+// - R0: True (-1) when the program finishes
 
-@R1
-D=M
-@ARRAYSTART
-M=D
-
-(LOOP)
-@LENGTH
-D=M
-@END
-D;JLE
-
-
-@ARRAYSTART
-D=M
-@I
-M=D
-
-
-@LENGTH
-D=M
-@J
-M=D
-
-(SUBLOOP)
-@J
-D=M
-@ENDSUBLOOP
-D;JLE
-
-@I
-D=M
-
-@TEMP1
-M=D
-
-@I
-D=M
-@TEMP2
-M=D+1
-
-
-
-@TEMP1
-A=M
-D=M
-@TEMP3
-M=D
-
-@TEMP2
-A=M
-D=M
-@TEMP4
-M=D
-
-@TEMP3
-D=M
-@TEMP4
-D=D-M
-@SWAP
-D;JGT
-
-(NOSWAP)
-@I
-M=M+1
-@J
-M=M-1
-@SUBLOOP
-0;JMP
-
-(SWAP)
-@TEMP1
-A=M
-@TEMP4
-D=M
-M=D
-
-@TEMP2
-A=M
-@TEMP3
-D=M
-M=D
-
-@I
-M=M+1
-
-@J
-M=M-1
-
-@SUBLOOP
-0;JMP
-
-(ENDSUBLOOP)
-@ARRAYSTART
-M=M+1
-@LENGTH
-M=M-1
-@LOOP
-0;JMP
-
-(END)
+// Initialize R0 to True (-1)
 @R0
 M=-1
+
+// Load the array address into a pointer (R3)
+@R1
+D=M
+@R3
+M=D
+
+// Outer loop: iterate through the array
+(OUTER_LOOP)
+    // Set a flag to track if any swaps were made in this pass
+    @0
+    D=A
+    @SWAP_FLAG
+    M=D
+
+    // Inner loop: compare and swap adjacent elements
+    (INNER_LOOP)
+        // Load the current element (R3 points to it)
+        @R3
+        D=M
+        // Load the next element (R3+1 points to it)
+        @R3
+        A=M+1
+        D=D-M
+        // If D is positive or zero, no swap is needed, go to next iteration
+        @NO_SWAP
+        D;JGE
+
+        // Swap the elements
+        @R3
+        D=M
+        @R3
+        A=M+1
+        M=D
+        @R3
+        D=A
+        @R3
+        A=M+1
+        M=D
+
+        // Set the swap flag to True (1)
+        @1
+        D=A
+        @SWAP_FLAG
+        M=D
+
+    (NO_SWAP)
+        // Move to the next pair of elements
+        @R3
+        M=M+1
+        // Check if we've reached the end of the array
+        @R2
+        D=M
+        @R3
+        D=D-A
+        // If we haven't reached the end, continue the inner loop
+        @INNER_LOOP
+        D;JNE
+
+    // If no swaps were made in this pass, the array is sorted
+    @SWAP_FLAG
+    D=M
+    @OUTER_LOOP
+    D;JEQ
+
+// End of the program
+@END
+0;JMP
