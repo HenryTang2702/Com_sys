67aa39a81a2c2ee5cda2206564709bca68095b96
HenryTang2702 <copsuilat1237890@gmail.com>
2023-09-27 03:40:00 +0930

    sdads

--- prac5/Mult/Mult.asm
+++ prac5/Mult/Mult.asm
@@ -8,50 +8,37 @@
 
 // Put your code here.
 
-// Mult.asm - Multiply two numbers and store the result in R0
-
-// Initialize R0 to 0
 @R0
 M=0
 
-// Load the first number into D (R1)
 @R1
 D=M
+@STEP
+D;JGT
 
-// Load the second number into R2
-@R2
-M=D
 
-// Initialize a counter (R3) to the second number
-@R3
-M=0
-
-(LOOP)
-   // Check if the counter (R3) is equal to the second number
-@R3
-D=M
-@R2
-D=D-M
 @END
-D;JEQ
-
-// Add the first number (R1) to the result (R0)
-@R0
-D=M
-@R1
-D=D+M
-@R0
-M=D
-
-   // Increment the counter (R3) by 1
-@R3
-M=M+1
-
-// Repeat the loop
-@LOOP
 0;JMP
 
+
+(STEP)
+
+@R0
+D=M
+
+@R2
+D=D+M
+
+@R0
+M=D
+
+@R1
+D=M-1
+M=D
+
+@STEP
+D;JGT
+
 (END)
-// End of program
 @END
 0;JMP
