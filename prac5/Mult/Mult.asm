9e6267903b68ec6c0f09cc8c34ad3e026359b8ef
HenryTang2702 <copsuilat1237890@gmail.com>
2023-09-27 03:44:53 +0930

    sdads

--- prac5/Mult/Mult.asm
+++ prac5/Mult/Mult.asm
@@ -8,36 +8,75 @@
 
 // Put your code here.
 
+// @R0
+// M=0
+
+// @R1
+// D=M
+// @STEP
+// D;JGT
+
+
+// @END
+// 0;JMP
+
+
+// (STEP)
+
+// @R0
+// D=M
+
+// @R2
+// D=D+M
+
+// @R0
+// M=D
+
+// @R1
+// D=M-1
+// M=D
+
+// @STEP
+// D;JGT
+
+// (END)
+// @END
+// 0;JMP
+
+// Initialize R0 to 0
 @R0
 M=0
 
+// Load R1 into D
 @R1
 D=M
-@STEP
-D;JGT
 
-
-@END
-0;JMP
-
+// Check if R2 is negative
+@R2
+D;JLT
+// If R2 is negative, negate it and set a flag in R3
+@R3
+M=0
+@R2
+D=M
+@R2
+M=-D
+@R3
+M=1
 
 (STEP)
 
+// If R2 is not zero, add R1 to R0 and decrement R2
 @R0
 D=M
-
 @R2
-D=D+M
-
+D;JNE
+// If R2 is zero, check if it was originally negative
+@R3
+D;JEQ
+// If R2 was originally negative, negate the result in R0
 @R0
-M=D
-
-@R1
-D=M-1
-M=D
-
-@STEP
-D;JGT
+M=-M
 
 (END)
 @END
