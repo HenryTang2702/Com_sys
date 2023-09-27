a45292ac35e617ee19892e09898a1104ad16dcfd
HenryTang2702 <copsuilat1237890@gmail.com>
2023-09-28 00:36:06 +0930

    sdads

--- prac5/ArrMin/ArrMin.asm
+++ prac5/ArrMin/ArrMin.asm
@@ -3,56 +3,126 @@
 
 // Put your code here.
 
+// @R1
+// D=M
+// @R3
+// M=D
+
+// @R2
+// D=M
+// @R4
+// M=D
+
+// @R3
+// A=M
+// D=M
+// @R0
+// M=D
+
+// @R3
+// M=M+1
+// @R4
+// M=M-1
+
+// (LOOP)
+// @R4
+// D=M
+// @END
+// D;JEQ
+
+// @R3
+// A=M
+// D=M
+
+// @R0
+// D=D-M
+// @SKIP
+// D;JGE
+
+// @R3
+// A=M
+// D=M
+// @R0
+// M=D
+
+// (SKIP)
+// @R3
+// M=M+1
+// @R4
+// M=M-1
+// @LOOP
+// 0;JMP
+
+// (END)
+// @END
+// 0;JMP
+
 @R1
-D=M
-@R3
-M=D
+    D=M-1
+    @R2
+    M=M+D
 
-@R2
-D=M
-@R4
-M=D
-
-@R3
-A=M
-D=M
-@R0
-M=D
-
-@R3
-M=M+1
-@R4
-M=M-1
+    @32767
+    D=A
+    @R0
+    M=D
 
 (LOOP)
-@R4
-D=M
-@END
-D;JEQ
-
-@R3
-A=M
-D=M
-
-@R0
-D=D-M
-@SKIP
-D;JGE
-
-@R3
-A=M
-D=M
-@R0
-M=D
-
+(CHECK_TERMINATE)
+	@R1
+	D=M
+	@R2
+	D=D-M
+	@END
+	D;JGT
+	@R1
+	A=M
+	D=M
+    @ELEM_POS
+    D;JGE
+	@ELEM_NEG
+	0;JMP
+(UPDATE)
+	@R1
+	A=M
+	D=M
+	@R0
+	M=D
 (SKIP)
-@R3
-M=M+1
-@R4
-M=M-1
-@LOOP
-0;JMP
-
+	@R1
+	M=M+1
+	@LOOP
+	0;JMP
 (END)
-@END
-0;JMP
+	@END
+	0;JMP
+
+(R0_NEG)
+    
+(R0_POS)
+    // subs
+    @R1
+	A=M
+	D=M
+	@R0
+	D=D-M // substraction, may cause Overflow!
+    @SKIP
+	D;JGE
+    @UPDATE
+    0;JMP
+
+(ELEM_NEG)
+    @R0
+    D=M
+    @R0_NEG
+    D;JLT
+    @UPDATE
+    0;JMP
+
+(ELEM_POS)
+    @R0
+    D=M
+    @R0_POS
+    D;JGE
+    @SKIP
+    0;JMP
--- prac5/Mult/Mult.asm
+++ prac5/Mult/Mult.asm
@@ -8,37 +8,116 @@
 
 // Put your code here.
 
-@R0
-M=0
+// @R0
+// M=0
 
-@R1
-D=M
-@STEP
-D;JGT
-
-
-@END
-0;JMP
+// @R1
+// D=M
+// @STEP
+// D;JGT
 
 
-(STEP)
+// @END
+// 0;JMP
 
-@R0
-D=M
 
-@R2
-D=D+M
+// (STEP)
 
-@R0
-M=D
+// @R0
+// D=M
 
-@R1
-D=M-1
-M=D
+// @R2
+// D=D+M
 
-@STEP
-D;JGT
+// @R0
+// M=D
+
+// @R1
+// D=M-1
+// M=D
+
+// @STEP
+// D;JGT
+
+// (END)
+// @END
+// 0;JMP
+
+    @sign
+    M=0
+(CHECK_R1)
+    @R1
+    D=M
+    @ABS_R1
+    D;JLT
+(CHECK_R2)
+    @R2
+    D=M
+    @ABS_R2
+    D;JLT
+
+(ENTRY)
+    @R1
+    D=M
+    @R2
+    D=D-M
+    @SWAP_R1R2
+    D;JLT
+
+(LOOP)
+    @R2
+    MD=M-1
+    @CHECK_SIGN
+    D;JLT
+    @R1
+    D=M
+    @R0
+    M=M+D
+    @LOOP
+    0;JMP
+
+(CHECK_SIGN)
+    @sign
+    D=M
+    @END
+    D;JEQ
+    @R0
+    M=-M
+    @END
+    0;JMP
 
 (END)
-@END
-0;JMP
+    @END
+    0;JMP
+
+(ABS_R2)
+    @sign
+    M=!M
+    @R2
+    M=-M
+    @ENTRY
+    0;JMP
+
+(ABS_R1)
+    @sign
+    M=!M
+    @R1
+    M=-M
+    @CHECK_R2
+    0;JMP
+
+(SWAP_R1R2)
+    @R1
+    D=M
+    @temp
+    M=D
+    @R2
+    D=M
+    @R1
+    M=D
+    @temp
+    D=M
+    @R2
+    M=D
+    @LOOP
+    0;JMP
