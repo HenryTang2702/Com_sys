34ec4fc5e71b220d93b4a06f6f70d22753db2dd3
HenryTang2702 <copsuilat1237890@gmail.com>
2023-09-30 23:06:50 +0930

    sdads

--- prac5/Mult/Mult.asm
+++ prac5/Mult/Mult.asm
@@ -42,82 +42,56 @@
 // (END)
 // @END
 // 0;JMP
+@R0      // Initialize R0 to 0
+M=0
+@3
+M=0
 
-    @sign
-    M=0
-(CHECK_R1)
-    @R1
-    D=M
-    @ABS_R1
-    D;JLT
-(CHECK_R2)
-    @R2
-    D=M
-    @ABS_R2
-    D;JLT
+@R1
+D=M
+@Positive
+D;JGE
 
-(ENTRY)
-    @R1
-    D=M
-    @R2
-    D=D-M
-    @SWAP_R1R2
-    D;JLT
+@3
+M=1
+// make R1 to be positive
+@R1
+M=-M
+
+(Positive)
 
 (LOOP)
-    @R2
-    MD=M-1
-    @CHECK_SIGN
+    @R1      // If the first number is 0, exit the loop
+    M=M-1
+    D=M
+    @ENDLOOP
     D;JLT
-    @R1
+
+    @R2
     D=M
     @R0
     M=M+D
-    @LOOP
-    0;JMP
+@LOOP    // Repeat the loop
+0;JMP
 
-(CHECK_SIGN)
-    @sign
-    D=M
-    @END
-    D;JEQ
-    @R0
-    M=-M
-    @END
-    0;JMP
+(ENDLOOP)
+
+@3
+D=M
+@NoNeedTimesNegative
+D;JEQ
+
+@R0
+M=-M
+
+(NoNeedTimesNegative)
+
+
+
+// Halt the program
+@END
+0;JMP
 
 (END)
-    @END
-    0;JMP
-
-(ABS_R2)
-    @sign
-    M=!M
-    @R2
-    M=-M
-    @ENTRY
-    0;JMP
-
-(ABS_R1)
-    @sign
-    M=!M
-    @R1
-    M=-M
-    @CHECK_R2
-    0;JMP
-
-(SWAP_R1R2)
-    @R1
-    D=M
-    @temp
-    M=D
-    @R2
-    D=M
-    @R1
-    M=D
-    @temp
-    D=M
-    @R2
-    M=D
-    @LOOP
-    0;JMP
+@END
+0;JMP
