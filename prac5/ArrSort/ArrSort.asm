2c4e0a92f8813427881b8455ca7228a090b9dfa3
HenryTang2702 <copsuilat1237890@gmail.com>
2023-09-30 23:08:27 +0930

    sdads

--- prac5/ArrSort/ArrSort.asm
+++ prac5/ArrSort/ArrSort.asm
@@ -19,74 +19,105 @@
 // Outputs:
 // - R0: True (-1) when the program finishes
 
-// Initialize R0 to True (-1)
-@R0
-M=-1
+@R1 // the address of arr[0]
+    D=M-1 // substract 1 because the array starts from arr[0]
+    @R2 // the length
+    M=M+D // now R2 is the address of the last element
 
-// Load the array address into a pointer (R3)
-@R1
-D=M
-@R3
-M=D
-
-// Outer loop: iterate through the array
 (OUTER_LOOP)
-    // Set a flag to track if any swaps were made in this pass
-    @0
-    D=A
-    @SWAP_FLAG
-    M=D
-
-    // Inner loop: compare and swap adjacent elements
-    (INNER_LOOP)
-        // Load the current element (R3 points to it)
-        @R3
-        D=M
-        // Load the next element (R3+1 points to it)
-        @R3
-        A=M+1
-        D=D-M
-        // If D is positive or zero, no swap is needed, go to next iteration
-        @NO_SWAP
-        D;JGE
-
-        // Swap the elements
-        @R3
-        D=M
-        @R3
-        A=M+1
-        M=D
-        @R3
-        D=A
-        @R3
-        A=M+1
-        M=D
-
-        // Set the swap flag to True (1)
-        @1
-        D=A
-        @SWAP_FLAG
-        M=D
-
-    (NO_SWAP)
-        // Move to the next pair of elements
-        @R3
-        M=M+1
-        // Check if we've reached the end of the array
-        @R2
-        D=M
-        @R3
-        D=D-A
-        // If we haven't reached the end, continue the inner loop
-        @INNER_LOOP
-        D;JNE
-
-    // If no swaps were made in this pass, the array is sorted
-    @SWAP_FLAG
+(CHECK_TERMINATE)
+    @R1
     D=M
-    @OUTER_LOOP
-    D;JEQ
+    @R2
+    D=D-M
+    @FINISH
+    D;JGT
+    @R1
+    D=M
+    @R3 // use R3 as the index of the inner loop.
+    M=D+1 
 
-// End of the program
-@END
-0;JMP
+
+(INNER_LOOP)
+(CHECK_INNER_END)
+    @R3
+    D=M
+    @R2
+    D=D-M
+    @INNER_FINISH
+    D;JGT
+    @R3 // use inner index to locate the element.
+    A=M
+    D=M // now D contains the element pointed by the inner index.
+    @ELEM_POS
+    D;JGE
+    @ELEM_NEG
+    0;JMP
+(SWAP) // swap the value pointed by the inner and outer index pointer
+    @R1
+    A=M
+    D=M
+    @temp
+    M=D
+    @R3
+    A=M
+    D=M
+    @R1
+    A=M
+    M=D
+    @temp
+    D=M
+    @R3
+    A=M
+    M=D
+(SKIP)
+    @R3
+    M=M+1
+    @INNER_LOOP
+    0;JMP
+
+(INNER_FINISH)
+    @R1
+    M=M+1
+    @OUTER_LOOP
+    0;JMP
+
+(FINISH)
+    @R0
+    M=-1
+(END)
+    @END
+    0;JMP
+
+(REF_NEG)
+    
+(REF_POS)
+    // subs
+    @R3
+    A=M
+    D=M
+    @R1
+    A=M
+    D=D-M // substraction, may cause Overflow!
+    @SKIP
+    D;JGE
+    @SWAP
+    0;JMP
+
+(ELEM_NEG)
+    @R1
+    A=M
+    D=M
+    @REF_NEG
+    D;JLT
+    @SWAP
+    0;JMP
+
+(ELEM_POS)
+    @R1
+    A=M
+    D=M
+    @REF_POS
+    D;JGE
+    @SKIP
+    0;JMP
