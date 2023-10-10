a8ceaced5875af271fa494f6bb837fe2d2c591fd
HenryTang2702 <copsuilat1237890@gmail.com>
2023-10-11 03:17:12 +1030

    dsads

--- prac6/VMTranslator/VMTranslator.py
+++ prac6/VMTranslator/VMTranslator.py
@@ -39,106 +39,30 @@ class VMTranslator:
 
     def vm_pop(segment, offset):
         '''Generate Hack Assembly code for a VM pop operation'''
-        if segment == "local":
-            return """
-            @{}
-            D=A
-            @LCL
-            D=M+D
-            @R13
-            M=D
+        if segment in ["static", "pointer", "temp"]:
+            code = f"""
             @SP
             AM=M-1
             D=M
-            @R13
-            A=M
+            @{segment.upper()}.{offset}
             M=D
-            """.format(offset)
-        elif segment == "argument":
-            return """
-            @{}
-            D=A
-            @ARG
-            D=M+D
-            @R13
-            M=D
-            @SP
-            AM=M-1
-            D=M
-            @R13
-            A=M
-            M=D
-            """.format(offset)
-        elif segment == "this":
-            return """
-            @{}
-            D=A
-            @THIS
-            D=M+D
-            @R13
-            M=D
-            @SP
-            AM=M-1
-            D=M
-            @R13
-            A=M
-            M=D
-            """.format(offset)
-        elif segment == "that":
-            return """
-            @{}
-            D=A
-            @THAT
-            D=M+D
-            @R13
-            M=D
-            @SP
-            AM=M-1
-            D=M
-            @R13
-            A=M
-            M=D
-            """.format(offset)
-        elif segment == "temp":
-            return """
-            @{}
-            D=A
-            @5
-            D=A+D
-            @R13
-            M=D
-            @SP
-            AM=M-1
-            D=M
-            @R13
-            A=M
-            M=D
-            """.format(offset)
-        elif segment == "pointer":
-            return """
-            @{}
-            D=A
-            @3
-            D=A+D
-            @R13
-            M=D
-            @SP
-            AM=M-1
-            D=M
-            @R13
-            A=M
-            M=D
-            """.format(offset)
-        elif segment == "static":
-            return """
-            @SP
-            AM=M-1
-            D=M
-            @{}.{}
-            M=D
-            """.format(sys.argv[1][:-3], offset)
+            """
         else:
-            return ""
+            code = f"""
+            @{offset}
+            D=A
+            @{segment.upper()}
+            D=D+M
+            @R5
+            M=D
+            @SP
+            AM=M-1
+            D=M
+            @R5
+            A=M
+            M=D
+            """
+        return code
         
 
     def vm_add():
